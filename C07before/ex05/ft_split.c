4ffd31f8f921542480942408becaa2df899a6519
Yusuf <yusuftaha9@gmail.com>
2021-09-20 11:22:37 +0300

    New ft_split.c file

--- C07before/ex05/ft_split.c
+++ C07before/ex05/ft_split.c
@@ -1,56 +1,83 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-_Bool	is_sep(char *c, char *charset)
+int	is_a_err(char ch, char *base)
 {
-	while (*charset != 0)
+	while (*base != 0)
 	{
-		if (*charset == *c)
-		{
+		if (*base == ch)
 			return (1);
-		}
-		charset++;
+		base++;
 	}
 	return (0);
 }
 
-int	split(char *str, char *charset, int w_c, char **arr)
+int	strl(char *str, char *base)
 {
-	int		cs_i;
-	int		w_i;
-	int		b_i;
-	_Bool	safemode;
+	int	i;
 
-	safemode = (w_c == -1);
-	w_c = 0;
-	cs_i = 0;
-	while (str[cs_i] != 0)
+	i = 0;
+	while (str[i] != 0 && !is_a_err(str[i], base))
+		i++;
+	return (i);
+}
+
+int	find_str(char *str, char *base)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	c = 0;
+	while (*str != 0)
 	{
-		while (is_sep(str + cs_i, charset) && str[cs_i] != 0)
-			cs_i++;
-		w_i = 0;
-		while (str[cs_i + w_i] != 0 && !is_sep(str + cs_i + w_i, charset))
-			w_c += (w_i++ == 0);
-		b_i = 0;
-		if (w_i != 0 && !safemode)
-			arr[w_c - 1] = (char *)malloc(w_i + 1);
-		if (safemode)
-			cs_i = cs_i + w_i;
-		else
-			while (w_i != 0 && str[cs_i] != 0 && !is_sep(str + cs_i, charset))
-				arr[w_c - 1][b_i++] = str[cs_i++];
+		while (is_a_err(*str, base) && *str != 0)
+			str++;
+		if (*str == 0)
+			return (c);
+		c++;
+		while (!is_a_err(*str, base) && *str != 0)
+			str++;
 	}
-	return (w_c);
+	return (c);
+}
+
+char	*strcopy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (*src != 0 && i < n)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		i++;
+	}
+	*dest = 0;
+	return (dest);
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	int		word_count;
-	char	**arr;
+	char	**str_m;
+	int		c;
+	int		i;
 
-	word_count = split(str, charset, -1, 0);
-	arr = (char **)malloc(sizeof(char *) * (word_count + 1));
-	arr[word_count] = 0;
-	split(str, charset, 0, arr);
-	return (arr);
+	i = 0;
+	c = find_str(str, charset);
+	str_m = malloc(sizeof(char *) * c + 1);
+	while (i < c)
+	{
+		while (is_a_err(*str, charset) && *str != 0)
+			str++;
+		if (*str == 0)
+			return (str_m);
+		str_m[i] = malloc(strl(str, charset) + 1);
+		strcopy(str_m[i], str, strl(str, charset));
+		while (!is_a_err(*str, charset) && *str != 0)
+			str++;
+		i++;
+	}
+	str_m[c] = 0;
+	return (str_m);
 }
